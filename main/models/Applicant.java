f130446252ec084a659acaa2ec399e437704507f
thePPH <130406hungcg@gmail.com>
2025-04-03 21:56:15 +0800

    Update Applicant.java

--- main/models/Applicant.java
+++ main/models/Applicant.java
@@ -1,1 +1,10 @@
+package models;
 
+public class Applicant extends User {
+    public Applicant(String nric, String password, int age, String maritalStatus) {
+        super(nric, password, age, maritalStatus);
+    }
+
+    @Override
+    public String getRole() { return "Applicant"; }
+}
