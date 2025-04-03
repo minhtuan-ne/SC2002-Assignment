72045c2adfd4b28b3f3e1776925bb48479bd6239
minhtuan-ne <phamtranminhtuan2006@gmail.com>
2025-04-03 21:49:18 +0700

    Update FileManager.java
    Change to HashMap

--- main/util/FileManager.java
+++ main/util/FileManager.java
@@ -1,61 +1,54 @@
 package main.util;
 
-import java.io.File;  
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
-import java.util.Scanner; 
+import java.util.Scanner;
 
 public class FileManager {
-      private List<List<String>> FileList = new ArrayList<>();  
+  private HashMap<String, List<List<String>>> DatabyRole = new HashMap<String, List<List<String>>>();
 
-      public List<List<String>> readFile(String fileName) {  
-        try {
-            File myObj = new File("./data/"+fileName);
-            Scanner myReader = new Scanner(myObj);
-            
-            boolean isFirstLine = true; 
-            while (myReader.hasNextLine()) {
-                String data = myReader.nextLine().trim(); 
-                
-                if (isFirstLine) { 
-                    isFirstLine = false;
-                    continue;
-                }
-                
-                if (!data.isEmpty()) { 
-                  String[] splitData = data.split("\\s+"); 
-                  List<String> applicantData = new ArrayList<>();
-                  
-                  for (String field : splitData) {
-                      applicantData.add(field); 
-                  }
-                  
-                  FileList.add(applicantData); 
-              }
-          }
-            myReader.close();
-        } catch (FileNotFoundException e) {
-            System.out.println("An error occurred.");
-            e.printStackTrace();
+  public List<List<String>> readFile(String fileName) {
+    List<List<String>> FileList = new ArrayList<>();
+    try {
+      File myObj = new File("./data/" + fileName);
+      Scanner myReader = new Scanner(myObj);
+
+      boolean isFirstLine = true;
+      while (myReader.hasNextLine()) {
+        String data = myReader.nextLine().trim();
+
+        if (isFirstLine) {
+          isFirstLine = false;
+          continue;
         }
-        return FileList;  
+
+        if (!data.isEmpty()) {
+          String[] splitData = data.split("\\s+");
+          List<String> applicantData = new ArrayList<>();
+
+          for (String field : splitData) {
+            applicantData.add(field);
+          }
+
+          FileList.add(applicantData);
+        }
+      }
+      myReader.close();
+    } catch (FileNotFoundException e) {
+      System.out.println("An error occurred.");
+      e.printStackTrace();
     }
+    return FileList;
+  }
 
-      public List<List<String>> getApplicantList() {  
-        return readFile("ApplicantList.txt");
-      }
+  public HashMap<String, List<List<String>>> getDatabyRole() {
+    DatabyRole.put("Applicant", readFile("ApplicantList.txt"));
+    DatabyRole.put("Manager", readFile("ManagerList.txt"));
+    DatabyRole.put("Officer", readFile("OfficerList.txt"));
+    DatabyRole.put("Project", readFile("ProjectList.txt"));
 
-      public List<List<String>> getManagerList() {  
-        return readFile("ManagerList.txt");
-      }
-
-      public List<List<String>> getOfficerList() {  
-        return readFile("OfficerList.txt");
-      }
-
-      public List<List<String>> getProjectList() {  
-        return readFile("ProjectList.txt");
-      }
-      
-}
+    return DatabyRole;
+  }
