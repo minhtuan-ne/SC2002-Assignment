069bd638269515a8e78c5358bab03cf4ef89c7d0
minhtuan-ne <phamtranminhtuan2006@gmail.com>
2025-04-03 22:55:55 +0700

    Update BTOApp.java

--- main/BTOApp.java
+++ main/BTOApp.java
@@ -1,5 +1,33 @@
 package main;
 
+import main.util.Authenticator;
+
+
 public class BTOApp {
-    
+    public static void main(String[] args) {
+        // Log in and get user type (Applicant, Officer, or Manager)
+       Authenticator auth = new Authenticator();
+       String userType = auth.LogIn();
+        
+       // Function for each usertype
+       switch (userType) {
+        case "Applicant":
+            // Functions for Applicant
+            
+            break;
+
+        case "Officer":
+            // Functions for Applicant
+            
+            break;
+
+        case "Manager":
+            // Functions for Applicant
+            
+            break;
+
+        default:
+            break;
+       }
+    }
 }
