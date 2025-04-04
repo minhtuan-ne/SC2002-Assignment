f428acf052146fbce9cbf03fe6598d264f7dd3e2
PhamHongPhuc2712 <phamhongphuc271206@gmail.com>
2025-04-05 01:26:24 +0800

    Update HDBManagerService.java

--- main/service/HDBManagerService.java
+++ main/service/HDBManagerService.java
@@ -8,7 +8,7 @@ import main.models.ProjectRepository;
 
 public class HDBManagerService {
     // create a new project
-    public boolean createProject(HDBManager manager, String name, String neighborhood, Date startDate, Date endDate, ArrayList<String> flatTypes, int twoRoomUnits, int threeRoomUnits){
+    public static boolean createProject(HDBManager manager, String name, String neighborhood, Date startDate, Date endDate, ArrayList<String> flatTypes, int twoRoomUnits, int threeRoomUnits){
         ArrayList<BTOProject> existingProjects = manager.getProjects();
         for(BTOProject project : existingProjects){
             // check is there any overlapping between this project and current projects
@@ -23,24 +23,24 @@ public class HDBManagerService {
     }
 
     // view all projects 
-    public ArrayList<BTOProject> viewAllProjects(){
+    public static ArrayList<BTOProject> viewAllProjects(){
         ProjectRepository.getAllProjects();
     }
 
     // view manager's own project
-    public ArrayList<BTOProject> viewOwnProjects(HDBManager manager){
+    public static ArrayList<BTOProject> viewOwnProjects(HDBManager manager){
         return manager.getProjects();
     }
 
     // change project visibility
-    public void toggleVisibility(HDBManager manager, BTOProject project, boolean visibility){
+    public static void toggleVisibility(HDBManager manager, BTOProject project, boolean visibility){
         if(project.getManager().equals(manager)){
             project.setVisibility(visibility);
         }
     }
 
     // edit a BTO project
-    public void editBTOProject(HDBManager manager, BTOProject project, String newName, String newNeighborhood, Date newStartDate, Date newEndDate, ArrayList<String> flatTypes, int newTwoRoomUnits, int newThreeRoomUnits){
+    public static void editBTOProject(HDBManager manager, BTOProject project, String newName, String newNeighborhood, Date newStartDate, Date newEndDate, ArrayList<String> flatTypes, int newTwoRoomUnits, int newThreeRoomUnits){
         if(project.getManager().equals(manager)){
             project.setProjectName(newName);
             project.setNeighborhood(newNeighborhood);
@@ -52,7 +52,7 @@ public class HDBManagerService {
     }
 
     // delete a project
-    public void deleteBTOProject(HDBManager manager, BTOProject project){
+    public static void deleteBTOProject(HDBManager manager, BTOProject project){
         if(project.getManager().equals(manager)){
             manager.removeProject(project);
             ProjectRepository.removeProject(project);
@@ -60,7 +60,7 @@ public class HDBManagerService {
     }
 
     // officer registration
-    public boolean handleOfficerRegistration(HDBManager manager, BTOProject project, HDBOfficer officer){
+    public static boolean handleOfficerRegistration(HDBManager manager, BTOProject project, HDBOfficer officer){
         if(!project.getManager().equals(manager)){
             return false;
         }
@@ -72,55 +72,64 @@ public class HDBManagerService {
     }
 
     // handle BTO application
-    public boolean handleBTOApplication(HDBManager manager, Application application){
-        BTOProject project = application.getProject();
-        if (!project.getManager().equals(manager)) {
-            return false;
-        }
-        String flatType = application.getFlatType();
-        int available = project.getUnits(flatType);
-        if(available > 0){
-            return true;
+    public static boolean handleBTOApplication(HDBManager manager, Application application){
+        String projectName = application.getProjectName();
+        ArrayList<BTOProject> all = ProjectRepository.getAllProjects();
+        for(BTOProject project : all){
+            if(project.getProjectName().equalsIgnoreCase(projectName)){
+                if (!project.getManager().equals(manager)) {
+                    return false;
+                }
+                String flatType = application.getFlatType();
+                int available = project.getUnits(flatType);
+                if(available > 0){
+                    return true;
+                }
+            }
         }
         return false;
     }
 
     // handle withdrawal request
-    public void handleWithdrawal(HDBManager manager, BTOApplication application, boolean approve){
-        BTOProject project = application.getProject();
-        if (project.getManager().equals(manager)) {
-            if (approve) {
-                String status = application.getApplicationStatus();
-                if ("Successful".equals(status) || "Booked".equals(status)) {
-                    // restore the flat unit
-                    String flatType = application.getFlatType();
-                    int current = project.getUnits(flatType);
-                    project.setUnits(flatType, current + 1);
+    public static void handleWithdrawal(HDBManager manager, BTOApplication application){
+        BTOProject projectName = application.getProjectName();
+        ArrayList<BTOProject> all = ProjectRepository.getAllProjects();
+        for(BTOProject project : all){
+            if (project.getManager().equals(manager)) {
+                if(project.getProjectName().equalsIgnoreCase(application.getProjectName())){
+                    String status = application.getApplicationStatus();
+                    if ("Successful".equalsIgnoreCase(status) || "Booked".equalsIgnoreCase(status)) {
+                        // restore the flat unit
+                        String flatType = application.getFlatType();
+                        int current = project.getUnits(flatType);
+                        project.setUnits(flatType, current + 1);
+                    }
+                    application.setApplicationStatus("Withdrawn");
                 }
-                application.setApplicationStatus("Withdrawn");
-            } else {
-                // haven't think of any case for rejecting withdrawal request
             }
         }
     }
 
-    // generate a report base on 2 filters: room type and marital status
-    public void bookingReport(HDBManager manager, String filter){
-        ArrayList<BTOProject> all = ProjectRepository.getAllProjects();
-        if(filter.equals("Room type")){
+    // generate a report base on 2 filters: 2-room and 3-room
+    public static void bookingReport(HDBManager manager, String filter){
+        ArrayList<BTOProject> all = manager.getProjects();
+        if(filter.equalsIgnoreCase("2-room")){
             for(BTOProject project: all){
                 for(Application application : project.getApplications()){
-                    if("Booked".equals(application.getApplicationStatus())){
-                        if("")
-                        System.out.println("Applicant: " + application.getApplicant().getNRIC() + ", Flat type: " + application.getFlatType() + ", Project name: " + project.getProjectName() + ", Age: " + application.getApplicant().getAge() + ", Marital status: " + application.getApplicant().getMaritalStatus());
+                    if("Booked".equalsIgnoreCase(application.getApplicationStatus())){
+                        if(application.getFlatType().equalsIgnoreCase("2-room")){
+                            System.out.println("Applicant: " + application.getApplicant().getNRIC() + ", Flat type: " + application.getFlatType() + ", Project name: " + project.getProjectName() + ", Age: " + application.getApplicant().getAge() + ", Marital status: " + application.getApplicant().getMaritalStatus());
+                        }
                     }
                 }
             }
         } else {
             for(BTOProject project: all){
                 for(Application application : project.getApplications()){
-                    if("Married".equals(application.getApplicationStatus())){
-                        System.out.println("Applicant: " + application.getApplicant().getNRIC() + ", Flat type: " + application.getFlatType() + ", Project name: " + project.getProjectName() + ", Age: " + application.getApplicant().getAge() + ", Marital status: " + application.getApplicant().getMaritalStatus());
+                    if("Booked".equalsIgnoreCase(application.getApplicationStatus())){
+                        if(application.getFlatType().equalsIgnoreCase("3-room")){
+                            System.out.println("Applicant: " + application.getApplicant().getNRIC() + ", Flat type: " + application.getFlatType() + ", Project name: " + project.getProjectName() + ", Age: " + application.getApplicant().getAge() + ", Marital status: " + application.getApplicant().getMaritalStatus());
+                        }
                     }
                 }
             }
